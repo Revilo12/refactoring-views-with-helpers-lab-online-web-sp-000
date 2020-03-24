e517074eb52ed342ca802c92d5d5835a9f15657a
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-24 20:31:14 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,6 +2,7 @@ module SongsHelper
   def display_artist(song)
     if !!song.artist
       link_to song.artist_name, song.artist
-    end
+    else
+      link_to "Add Artist", "/songs/#{song.id}/edit"
   end
 end
